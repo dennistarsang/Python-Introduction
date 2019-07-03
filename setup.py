1b803fcdaf818798064eb1e3c93785172c248f0b
Dennis Sang <sangdennis9@gmail.com>
2019-07-03 15:14:51 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='palindrome',
+    version='1.0',
+    py_modules=['palindrome'],
+
+    #Metadata
+    author='Sang K. Dennis',
+    author_email='sangdennis9@gmail.com',
+    description='A module for finding palindromic numbers.',
+    license='Public domain',
+    keywords='example',
+)
